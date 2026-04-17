b2276df12b0d47faf139a7e6e74ae0d49c503778
Breno IFPB <brenomine2.0@gmail.com>
2026-04-17 10:43:00 -0300

    tividades

---
+++ 1P/atividades_6/02_maiornumero.py
@@ -0,0 +1,6 @@
+num = 1
+numeros = []
+while num > 0:
+    num = int(input())
+    numeros.append(num)
+print(max(numeros))
---
+++ 1P/atividades_6/03_éprimo.py
@@ -0,0 +1,11 @@
+primo = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229]
+num = 0
+sequencia = []
+
+while num > -1:
+    num = int(input())
+    if num not in primo:
+        sequencia.append(0)
+    else:
+        sequencia.append(1)
+print(sequencia)
